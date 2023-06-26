8262f072c0e64cf2805d2bb837d5e810a662d137
ikede-divine-keno <ikededivine@gmail.com>
2023-06-26 09:43:47 -0700

    0-safe_print_list.py modified

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for value in range(0, x):
-            print(my_list[value], end='')
+        for val in range(0, x):
+            print(my_list[val], end='')
         print('')
         return (x)
     except:
         print('')
-        return (value)
+        return (val)
