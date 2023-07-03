e6153d55cca518cf5d49657ea30b9e6f7e6df293
ikede-divine-keno <ikededivine@gmail.com>
2023-07-03 08:25:51 -0700

    0-rectangle.py modified

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Rectangle module.
+
+This module contains a class that defines a rectangle.
+
+"""
 
 
 class Rectangle():
-    """empty class to define a rectangle."""
+    """Defines a rectangle."""
 
     pass
