59d5420a20cccf3dc3151c5cc14e81bafcd5732e
ikede-divine-keno <ikededivine@gmail.com>
2023-07-04 15:08:06 -0700

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+"""
+This is the say_my_name module.
+
+This module supplies one function, say_my_name().
+"""
 
 
 def say_my_name(first_name, last_name=""):
     """
-    prints a hello string for a name, handling input errors
+    Print My name is <first name> <last name>.
+
+    Args:
+        first_name (str): the first name.
+        last_name (str): the last name.
     """
-    if not isinstance(first_name, str):
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
