96f4998534df460af288b3e0761aae6a9841b763
ikede-divine-keno <ikededivine@gmail.com>
2023-07-11 13:31:02 -0700

    0-read_file.py modified

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,8 @@
 
 
 def read_file(filename=""):
-    """Takes in str filename to read it and print to stdout
+    """Takes in str filename to read it and
+    print to stdout
     """
 
     with open(filename, encoding="utf-8") as readFile:
