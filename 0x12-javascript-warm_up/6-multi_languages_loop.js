4e542cd8d2de31281e53aed21864a80dabf88eec
ikede-divine-keno <ikededivine@gmail.com>
2023-09-11 17:35:36 -0700

    6-multi_languages_loop.js mod

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by
-// using an array of string and a loop
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(langs[i]);
-}
+myVar.forEach((element) => {
+  console.log(element);
+});
