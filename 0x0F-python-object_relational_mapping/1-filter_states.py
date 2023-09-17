fadcd5e0e5611821a9fc9105a06b20726a7ad1cd
ikede-divine-keno <ikededivine@gmail.com>
2023-09-17 10:55:32 -0700

    1-filter_states.py modified again

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""script for use in getting all states from sql db
-"""
-import MySQLdb
+"""  list all states based on the first name  """
 import sys
+import MySQLdb
 
-
-if __name__ == '__main__':
-    args = sys.argv
-    username = args[1]
-    password = args[2]
-    data = args[3]
-    db = MySQLdb.connect(host='localhost', user=username,
-                         passwd=password, db=data,
-                         port=3306)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    num_rows = cur.execute('''
-            SELECT * FROM states
-            WHERE states.name LIKE 'N%'
-            ORDER BY states.id
-            ''')
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY'N%'"
+                " ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
