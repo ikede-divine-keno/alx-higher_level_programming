7e0bb33dabc86c1ba203591622077ba1de4d691d
ikede-divine-keno <ikededivine@gmail.com>
2023-09-29 17:58:14 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(
+            "https://alx-intranet.hbtn.io/status") as response:
+        res = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode("utf8")))
