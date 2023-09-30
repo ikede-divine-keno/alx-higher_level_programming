54c7e749be494b62507ee1b4feb52471fcb8a099
ikede-divine-keno <ikededivine@gmail.com>
2023-09-29 19:31:07 -0700

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email
+as a parameter, and finally displays the body of the response."""
+import sys
+import requests
+
+if __name__ == "__main__":
+    param = {'email': sys.argv[2]}
+    print(requests.post(sys.argv[1], data=param).text)
