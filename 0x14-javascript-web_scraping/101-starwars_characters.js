9a3d1dc5fede65c9ac49f86c247fb25a5e688b7e
ikede-divine-keno <ikededivine@gmail.com>
2023-10-24 19:03:07 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,13 +4,20 @@ const request = require('request');
 request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body);
-    results.characters.forEach(element => {
-      request(element, function (error, response, body) {
-        if (!error) {
-          const list = JSON.parse(body);
-          console.log(list.name);
-        }
-      });
-    });
+    processcharacters(results.characters, 0);
   }
 });
+
+function processcharacters (characters, n) {
+  request(characters[n], function (error, res, body1) {
+    if (error) {
+      console.log(error);
+    }
+    const data1 = JSON.parse(body1);
+    console.log(data1.name);
+
+    if (n + 1 < characters.length) {
+      processcharacters(characters, n + 1);
+    }
+  });
+}
